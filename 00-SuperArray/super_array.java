30333c3f0fd3ea3377e95fb5a9c8e4f55d1d1d84
Joshua <jsiganur@gmail.com>
2021-02-03 08:33:32 -0500

    added a few more methods

--- 00-SuperArray/super_array.java
+++ 00-SuperArray/super_array.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
-//e.l
+
 public class superArray {
   private int[] array;
 
-  public superArray(int a){
+  public superArray(int a){ //makes the actual array where the values are inputted, this one is if a value is given
+    this.array = new int[a];
+    System.out.println(this.array.length);
+  }
+
+  public superArray(){ ////makes the actual array where the values are inputted, this one is if no value is given
     this.array = new int[10];
     System.out.println(this.array.length);
   }
 
-  public superArray(){
-    this.array = new int[10];
-    System.out.println(this.array.length);
-  }
-
-  public void add(int value){
+  public void add(int value){ //this adds the value the user inputs to the array
     for (int i = 0; i < this.array.length; i++){
       if (array[i] == 0){
         array[i] = value;
@@ -26,10 +26,14 @@ public class superArray {
   }
 
   public void add(int ind, int value){
-
+    grow(1); //which requires I use the grow method to increase the size of the array
+    for (int i = this.array.length - 1; i > ind; i--){
+      this.array[i] = this.array[i - 1];
+    }
+    this.array[ind] = value;
   }
 
-  public void grow(int b){
+  public void grow(int b){ //this "grows" the array by making a temporary one then moving the results to the new and bigger one
     int[] tempArray = new int[this.array.length];
     for (int i = 0; i < this.array.length; i++){
       tempArray[i] = this.array[i];
@@ -39,5 +43,9 @@ public class superArray {
     for (int i = 0; i < tempArray.length; i++){
       this.array[i] = tempArray[i];
     }
+  }
+
+  public void set(int x, int value){ //this sets the value at a specific spot in the array
+    this.array[x] = value;
   }
 }
